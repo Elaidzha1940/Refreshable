86174a5bcf990fe73d58ec95c08a3c4071a37783
Elaidzha1940 <eli080293@gmail.com>
2024-01-08 18:10:02 +0300

    Refreshable

---
+++ Refreshable/Refreshable.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: Refreshable
+//  File: Refreshable.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 08.01.2024
+//
+//  */
+  
+import SwiftUI
+
+struct Refreshable: View {
+    var body: some View {
+        
+        VStack {
+           
+        }
+    }
+}
+
+#Preview {
+    Refreshable()
+}
---
+++ Refreshable/RefreshableApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: Refreshable
+//  File: RefreshableApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 08.01.2024
+//
+//  */
+
+import SwiftUI
+
+@main
+struct RefreshableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Refreshable()
+        }
+    }
+}
