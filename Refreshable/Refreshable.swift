280e5dbd3efb2dc2563d0a72e0e19e91449b7729
Elaidzha1940 <eli080293@gmail.com>
2024-01-08 19:55:02 +0300

    Update Refreshable

--- Refreshable/Refreshable.swift
+++ Refreshable/Refreshable.swift
@@ -9,11 +9,48 @@
   
 import SwiftUI
 
+final class RefreshableDataService {
+    
+    func getData() async throws -> [String] {
+        ["Banana", "Melon", "Apple"].shuffled()
+    }
+}
+
+@MainActor
+final class RefreshableViewModel: ObservableObject {
+    @Published private(set) var items: [String] = []
+    let manager = RefreshableDataService()
+    
+    func loadData() {
+        Task {
+            do {
+                items = try await manager.getData()
+            } catch {
+                print(error)
+            }
+        }
+    }
+}
+
 struct Refreshable: View {
+    @StateObject private var viewModel = RefreshableViewModel()
+    
     var body: some View {
         
-        VStack {
-           
+        NavigationStack {
+            ScrollView {
+                VStack {
+                    ForEach(viewModel.items, id: \.self) { item in
+                        Text(item)
+                            .font(.headline)
+                    }
+                }
+                .navigationTitle("Refreshable")
+            }
+            .onAppear {
+                viewModel.loadData()
+                    
+            }
         }
     }
 }
